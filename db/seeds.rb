5e80a238edc1f3a0a3c3ad6291bf8c9bb6c117ec
Lucie Dispot <lucie.dispot@gmail.com>
2017-10-12 15:26:42 +0200

    Add second seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Song.create(title: "Power of Darkness (Instrumental)",  youtube_url: 'https://www.youtube.com/watch?v=HPoMypTAxkY', source: "youtube", author: "Two Steps From Hell", duration: 170, loop: false, attr_stress: 60, attr_epicness: 80, attr_happiness: 30, tag_list: "battle")
+Song.create(title: "My Freedom",  youtube_url: 'https://www.youtube.com/watch?v=w-y_fx_TNaw', source: "youtube", author: "Two Steps From Hell", duration: 237, loop: false, attr_stress: 0, attr_epicness: 60, attr_happiness: 80, tag_list: ["epic", "calm"])
