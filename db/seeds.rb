e5a18a0d1cfe003f52826e5149d8f39fc9f7bb6a
Lucie Dispot <lucie.dispot@gmail.com>
2017-10-13 23:00:21 +0200

    Remove irrelevant tags from youtube

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 Song.create(title: "Power of Darkness (Instrumental)",  youtube_url: 'https://www.youtube.com/watch?v=HPoMypTAxkY', source: "youtube", author: "Two Steps From Hell", duration: 170, loop: false, attr_stress: 60, attr_epicness: 80, attr_happiness: 30, tag_list: "battle")
 Song.create(title: "My Freedom",  youtube_url: 'https://www.youtube.com/watch?v=w-y_fx_TNaw', source: "youtube", author: "Two Steps From Hell", duration: 237, loop: false, attr_stress: 0, attr_epicness: 60, attr_happiness: 80, tag_list: ["epic", "calm"])
+Song.create(youtube_url: 'https://www.youtube.com/watch?v=ysrkvEds4I8', loop: false, attr_stress: 0, attr_epicness: 0, attr_happiness: 20)
