e54c0b78e8a1f462e4d9166cd76f34d661362d21
Arthur Poulet <arthur.poulet@sceptique.eu>
2017-10-13 11:51:03 +0200

    Add Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: "songs#index"
-  get "/songs/tag_cloud"
+  get "/", to: "dashboard#index", as: "root"
+  get "/songs/tag_cloud", as: "songs_tag_cloud"
   resources :songs
+  get "/dashboard", to: "dashboard#index", as: "dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
